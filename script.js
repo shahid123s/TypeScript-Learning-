bc01a1f34d11ebff9ba55df23bbebcb4584b80d5
shahid123s <shahidnoushad13s@gmail.com>
2025-04-20 22:52:46 +0530

    create an interface for user obj and displayed ;

---
+++ script.js
@@ -0,0 +1,8 @@
+var greeting = "Hello World!";
+console.log(greeting);
+var user = {
+    name: "Daniel",
+    age: 26,
+};
+var firstName = ' Daniel';
+// firstName = 666 // Error: Type 'number' is not assignable to type 'string'. it pops the error 
--- script.ts
+++ script.ts
@@ -1,3 +1,20 @@
 const greeting: string = "Hello World!";
 console.log(greeting);
 
+interface User  {
+    name: string;
+    age: number;
+}
+
+const user: User = {
+    name: "Daniel",
+    age: 26,
+};
+
+
+let firstName : string = ' Daniel';
+// firstName = 666 // Error: Type 'number' is not assignable to type 'string'. it pops the error 
+
+
+
+// 
